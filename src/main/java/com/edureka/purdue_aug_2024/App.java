302ceefd711188e32158f07d2bde20fa4fcb7d1f
Alak <alakd1985@gmail.com>
2024-09-02 19:19:15 -0400

    First commmit

---
+++ src/main/java/com/edureka/purdue_aug_2024/App.java
@@ -0,0 +1,13 @@
+package com.edureka.purdue_aug_2024;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
