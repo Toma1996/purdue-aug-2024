d0d2a4410280055ac4260d9dac5fafe6631589a1
Alak <alakd1985@gmail.com>
2024-09-06 10:26:20 -0400

    b11 first commit

---
+++ src/main/java/com/edureka/purdue_aug_2024/Login.java
@@ -0,0 +1,13 @@
+package com.edureka.purdue_aug_2024;
+
+/**
+ * Hello world!
+ *
+ */
+public class Login 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is login app" );
+    }
+}
