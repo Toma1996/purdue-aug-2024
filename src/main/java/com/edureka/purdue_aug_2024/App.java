1a73a3d7e4073c4a48c3a700c9dd88b20f57cbc5
Toma1996 <65564297+Toma1996@users.noreply.github.com>
2024-09-06 10:37:55 -0400

    Update App.java
    For practice purpose

--- src/main/java/com/edureka/purdue_aug_2024/App.java
+++ src/main/java/com/edureka/purdue_aug_2024/App.java
@@ -15,6 +15,11 @@ public class App
     public void b1() {
     	System.out.println( "This is b1 branch" );
 	}
+
+	 // changes in c1 branch
+    public void c1() {
+    	System.out.println( "This is c1 branch" );
+	}
 }
 
 
