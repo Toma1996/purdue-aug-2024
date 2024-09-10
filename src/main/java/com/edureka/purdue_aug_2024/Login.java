2f7744391233ec2d594ca5518a0bd483060a5b19
Alak <alakd1985@gmail.com>
2024-09-10 09:20:24 -0400

    changes from x branch

--- src/main/java/com/edureka/purdue_aug_2024/Login.java
+++ src/main/java/com/edureka/purdue_aug_2024/Login.java
@@ -4,21 +4,24 @@ package com.edureka.purdue_aug_2024;
  * Hello world!
  *
  */
-public class Login 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "This is login app" );
-    }
-    
-    
-    // from dev 1 and b111 branch
-    public void b1111() {
+public class Login {
+	public static void main(String[] args) {
+		System.out.println("This is login app");
+	}
+
+	// from dev 1 and b111 branch
+	public void b1111() {
 		System.out.println("Method -03");
 	}
 
 	// from dev 2 and y branch
-    public void m6() {
+	public void m6() {
 		System.out.println("from dev 2 and y branch");
+	}
+
+	// from dev 1 and x branch
+	public void m5() {
+		System.out.println("from dev 1 and x branch");
+
 	}
 }
